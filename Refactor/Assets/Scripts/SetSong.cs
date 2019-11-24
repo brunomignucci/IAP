ece7e030decc5a21387964819dd14310c973c285
juanmaPuhl <43391912+JuanmaPuhl@users.noreply.github.com>
2019-11-24 19:59:16 -0300

    Trigger de canciones como prefabs
    Agregar a la escena, asignar cancion y listo

---
+++ Refactor/Assets/Scripts/SetSong.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetSong : MonoBehaviour
+{   private bool activado;
+    public AudioClip cancion;
+    private bool personajeEncontrado;
+    private AudioManager manager;
+    // Start is called before the first frame update
+    void Start()
+    {
+        activado = false;
+        personajeEncontrado = false;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      if(!personajeEncontrado){
+        manager = GameObject.Find("Audio").GetComponent<AudioManager>();
+        if(manager != null){
+          personajeEncontrado = true;
+        }
+      }
+    }
+
+    void OnTriggerEnter(Collider other){
+      if(!activado){
+        activado = true;
+        manager.setNewSong(cancion);
+        Debug.Log("Cambio cancion");
+      }
+    }
+}
