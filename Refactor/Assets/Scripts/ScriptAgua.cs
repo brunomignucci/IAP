57f28ffbc16f242a9d120d6d9f6478a4e6340903
brunomignucci <brunomignucci@gmail.com>
2019-11-24 12:05:51 -0300

    Apagar bosque arbol por arbol, y luego de que se apaga el ultimo el soldado se corre y deja pasar

--- Refactor/Assets/Scripts/ScriptAgua.cs
+++ Refactor/Assets/Scripts/ScriptAgua.cs
@@ -25,8 +25,12 @@ public class ScriptAgua : MonoBehaviour
         RaycastHit hit;
         if (Physics.Raycast(rayo, out hit, 10.0f))
         {
-            if(hit.transform.parent!=null && hit.transform.parent.tag=="ARBOLES") GameObject.Find("Fuego").GetComponent<apagar_fuego>().apagar();
-            //Debug.Log("Apagando");
+            if (hit.transform.parent != null && hit.transform.parent.tag == "ARBOLES" && hit.transform.gameObject.tag == "FUEGO" && hit.transform.GetChild(0).gameObject.activeSelf == true)
+            {
+                GameObject.Find("Fuego").GetComponent<apagar_fuego>().apagar(hit.transform.gameObject);
+                //Debug.Log("Entro al if");
+            }
+            //else Debug.Log("No entra al if");
         }
         
 
--- Refactor/Assets/Scripts/apagar_fuego.cs
+++ Refactor/Assets/Scripts/apagar_fuego.cs
@@ -13,20 +13,25 @@ public class apagar_fuego : MonoBehaviour
     void Start()
     {
         contador = 0;
-        cantidad_hijos = this.transform.childCount;
+        cantidad_hijos = 10;
         //Debug.Log(cantidad_hijos);
     }
-    public void apagar()
+    public void apagar(GameObject fuego_a_apagar)
     {
-
-        if (contador % 20 == 0 && contador / 20 < cantidad_hijos)
+        contador++;
+        //Debug.Log("scrip afuera del if");
+        
+        if (contador % 20 == 0 && contador / 20 <= cantidad_hijos)
         {
             //Debug.Log("script_apagar");
-            this.transform.GetChild(contador / 20).gameObject.SetActive(false);
+            Debug.Log(contador);
+            fuego_a_apagar.transform.GetChild(0).gameObject.SetActive(false);
 
         }
-        contador++;
-        if (apague_todo == 0 && contador / 20 >= cantidad_hijos)
+        
+
+        //Debug.Log(contador);
+        if (apague_todo == 0 && contador / 20 == cantidad_hijos)
         {
             //habilitador_proximo_puzzle.transform.GetChild(1).gameObject.SetActive(false);
             habilitador_proximo_puzzle.transform.Translate(-10.0f, 0.0f, 0.0f);
