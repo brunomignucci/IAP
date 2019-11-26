c724d9b5563c8f1f16d2cbdc21fcc7060a33af85
brunomignucci <brunomignucci@gmail.com>
2019-11-26 12:02:42 -0300

    Creditos

---
+++ Refactor/Assets/Scripts/ActivadorCreditos.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActivadorCreditos : MonoBehaviour
+{
+    
+    GameObject credits;
+    bool encontre;
+    // Start is called before the first frame update
+    void Start()
+    {
+        encontre = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "DETECTORFINALISIMO" && !encontre) 
+        {
+            GameObject []  creditooos = GameObject.FindGameObjectsWithTag("CREDITOS");
+            if (creditooos.Length > 0)
+            {
+                credits = creditooos[0];
+                credits.transform.GetChild(0).gameObject.SetActive(true);
+                encontre = true;
+            }
+            
+        }
+    }
+}
---
+++ Refactor/Assets/Scripts/MoverCreditos.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoverCreditos : MonoBehaviour
+{
+    // Start is called before the first frame update
+    bool movi;
+    [SerializeField]
+    float velocidad;
+    void Start()
+    {
+        movi = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!movi)
+        {
+            movi = true;
+        }
+        if (movi)
+        {
+            this.transform.Translate(0, Time.deltaTime* velocidad, 0);
+        }
+    }
+}
