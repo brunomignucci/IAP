072abd57888054dedb38f5504fae789c8d1381cd
nlandau91 <43214457+nlandau91@users.noreply.github.com>
2019-11-27 13:59:47 -0300

    Reja

--- Refactor/Assets/Scripts/PuzzleManager.cs
+++ Refactor/Assets/Scripts/PuzzleManager.cs
@@ -1,93 +1,113 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Networking;
 
-public class PuzzleManager : MonoBehaviour
+public class PuzzleManager : NetworkBehaviour
 {
-    public GameObject pilar1;
-    public GameObject pilar2;
-    public GameObject pilar3;
-    public GameObject pilar4;
-    private bool completoPilar1;
-    private bool completoPilar2;
-    private bool completoPilar3;
-    private bool completoPilar4;
+	public GameObject pilar1;
+	public GameObject pilar2;
+	public GameObject pilar3;
+	public GameObject pilar4;
+	private bool completoPilar1;
+	private bool completoPilar2;
+	private bool completoPilar3;
+	private bool completoPilar4;
 
-    private GameObject rotationPanel1;
-    private GameObject rotationPanel2;
-    private GameObject rotationPanel3;
-    private GameObject rotationPanel4;
+	private GameObject rotationPanel1;
+	private GameObject rotationPanel2;
+	private GameObject rotationPanel3;
+	private GameObject rotationPanel4;
 
-    public Finalizador puerta;
+	public Finalizador puerta;
 
-    private bool completo;
-    // Start is called before the first frame update
-    void Start()
-    {
-      completo = completoPilar1 = completoPilar2 = completoPilar3 = completoPilar4 = false;
-      rotationPanel1 = pilar1.transform.GetChild(1).gameObject;
-      rotationPanel2 = pilar2.transform.GetChild(1).gameObject;
-      rotationPanel3 = pilar3.transform.GetChild(1).gameObject;
-      rotationPanel4 = pilar4.transform.GetChild(1).gameObject;
-    }
+	private bool completo;
+	// Start is called before the first frame update
+	void Start()
+	{
+		if (!isServer)
+		{
+			return;
+		}
+		completo = completoPilar1 = completoPilar2 = completoPilar3 = completoPilar4 = false;
+		rotationPanel1 = pilar1.transform.GetChild(1).gameObject;
+		rotationPanel2 = pilar2.transform.GetChild(1).gameObject;
+		rotationPanel3 = pilar3.transform.GetChild(1).gameObject;
+		rotationPanel4 = pilar4.transform.GetChild(1).gameObject;
+	}
 
-    // Update is called once per frame
-    void Update()
-    {
+	// Update is called once per frame
+	void Update()
+	{
+		if (!isServer)
+		{
+			return;
+		}
+		if (Input.GetKeyDown("p"))
+		{
+			Debug.Log(completoPilar1);
+			Debug.Log(completoPilar2);
+			Debug.Log(completoPilar3);
+			Debug.Log(completoPilar4);
+			Debug.Log(rotationPanel4.transform.rotation.eulerAngles.y);
+			completo = true;
+			puerta.abrirPuerta();
+		}
+		if (rotationPanel1.transform.localRotation.eulerAngles.y == 180f)
+		{
+			completoPilar1 = true;
+			//Debug.Log("Completado Pilar 1");
+			pilar1.transform.GetChild(2).gameObject.GetComponent<Renderer>().material.SetColor("_Color", Color.blue);
+		}
+		else
+		{
+			completoPilar1 = false;
+			pilar1.transform.GetChild(2).gameObject.GetComponent<Renderer>().material.SetColor("_Color", Color.white);
+		}
 
-      if(Input.GetKeyDown("p")){
-        Debug.Log(completoPilar1);
-        Debug.Log(completoPilar2);
-        Debug.Log(completoPilar3);
-        Debug.Log(completoPilar4);
-        Debug.Log(rotationPanel4.transform.rotation.eulerAngles.y);
-      }
-      if(rotationPanel1.transform.localRotation.eulerAngles.y == 180f){
-        completoPilar1 = true;
-          //Debug.Log("Completado Pilar 1");
-        pilar1.transform.GetChild(2).gameObject.GetComponent<Renderer>().material.SetColor("_Color", Color.blue);
-      }
-      else{
-        completoPilar1 = false;
-        pilar1.transform.GetChild(2).gameObject.GetComponent<Renderer>().material.SetColor("_Color", Color.white);
-      }
+		if (rotationPanel2.transform.localRotation.eulerAngles.y == 270f)
+		{
+			completoPilar2 = true;
+			// Debug.Log("Completado Pilar 2");
+			pilar2.transform.GetChild(2).gameObject.GetComponent<Renderer>().material.SetColor("_Color", Color.blue);
+		}
+		else
+		{
+			completoPilar2 = false;
+			pilar2.transform.GetChild(2).gameObject.GetComponent<Renderer>().material.SetColor("_Color", Color.white);
+		}
 
-      if(rotationPanel2.transform.localRotation.eulerAngles.y == 270f){
-        completoPilar2 = true;
-         // Debug.Log("Completado Pilar 2");
-        pilar2.transform.GetChild(2).gameObject.GetComponent<Renderer>().material.SetColor("_Color", Color.blue);
-      }
-      else{
-        completoPilar2 = false;
-        pilar2.transform.GetChild(2).gameObject.GetComponent<Renderer>().material.SetColor("_Color", Color.white);
-      }
+		if (rotationPanel3.transform.localRotation.eulerAngles.y == 90f)
+		{
+			completoPilar3 = true;
+			// Debug.Log("Completado Pilar 3");
+			pilar3.transform.GetChild(2).gameObject.GetComponent<Renderer>().material.SetColor("_Color", Color.blue);
+		}
+		else
+		{
+			completoPilar3 = false;
+			pilar3.transform.GetChild(2).gameObject.GetComponent<Renderer>().material.SetColor("_Color", Color.white);
+		}
 
-      if(rotationPanel3.transform.localRotation.eulerAngles.y == 90f){
-        completoPilar3 = true;
-         // Debug.Log("Completado Pilar 3");
-        pilar3.transform.GetChild(2).gameObject.GetComponent<Renderer>().material.SetColor("_Color", Color.blue);
-      }
-      else{
-        completoPilar3 = false;
-        pilar3.transform.GetChild(2).gameObject.GetComponent<Renderer>().material.SetColor("_Color", Color.white);
-      }
+		if (rotationPanel4.transform.localRotation.eulerAngles.y >= 0 && rotationPanel4.transform.localRotation.eulerAngles.y < 0.1)
+		{
+			completoPilar4 = true;
+			//Debug.Log("Completado Pilar 4");
+			pilar4.transform.GetChild(2).gameObject.GetComponent<Renderer>().material.SetColor("_Color", Color.blue);
+		}
+		else
+		{
+			completoPilar4 = false;
+			pilar4.transform.GetChild(2).gameObject.GetComponent<Renderer>().material.SetColor("_Color", Color.white);
+		}
 
-      if(rotationPanel4.transform.localRotation.eulerAngles.y >= 0 && rotationPanel4.transform.localRotation.eulerAngles.y < 0.1){
-        completoPilar4 = true;
-        //Debug.Log("Completado Pilar 4");
-        pilar4.transform.GetChild(2).gameObject.GetComponent<Renderer>().material.SetColor("_Color", Color.blue);
-      }
-      else{
-        completoPilar4 = false;
-        pilar4.transform.GetChild(2).gameObject.GetComponent<Renderer>().material.SetColor("_Color", Color.white);
-      }
-
-      if(completoPilar1 && completoPilar2 && completoPilar3 && completoPilar4 && !completo){
-       // Debug.Log("Puzzle Completo");
-        completo = true;
-        puerta.abrirPuerta();
-      }
-    }
+		if (completoPilar1 && completoPilar2 && completoPilar3 && completoPilar4 && !completo)
+		{
+			// Debug.Log("Puzzle Completo");
+			completo = true;
+			puerta.abrirPuerta();
+		}
+	}
 
 
 }
--- Refactor/Assets/Scripts/RotatePuzzle.cs
+++ Refactor/Assets/Scripts/RotatePuzzle.cs
@@ -2,126 +2,76 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Networking;
-using System;
+
 public class RotatePuzzle : NetworkBehaviour
 {
-    public GameObject pilar;
-    private GameObject toRotate;
-    private bool debounce;
-    private float velocidad;
-    private Quaternion target;
-    private float lastRotation;
-    private bool termineMov;
-    private bool flagRotacion;
-    private Vector3 Origin;
-    private bool captureRotacionTarget;
-    private Quaternion rotacionTarget,rotacionActual;
-    // Start is called before the first frame update
-    void Start()
-    {
-        if (!isServer)
-        {
-            return;
-        }
-        flagRotacion = false;
-        captureRotacionTarget = false;
-      velocidad = 5f;
-      debounce = false;
-      termineMov = false;
-      toRotate = pilar.transform.GetChild(1).gameObject;
-      //GetComponent<Renderer>().material.SetColor("_Color", Color.white);
-      GetComponent<SpawnableObject>().ChangeColor(Color.white);
-      target = Quaternion.Euler(0,toRotate.transform.localRotation.eulerAngles.y,0);
-      lastRotation = 0;
-    }
+	public GameObject pilar;
+	private GameObject toRotate;
+	private bool debounce;
+	private float velocidad;
+	private Quaternion target;
+	private float lastRotation;
+	private bool termineMov;
+	private Vector3 Origin;
+	// Start is called before the first frame update
+	void Start()
+	{
+		if (!isServer)
+		{
+			return;
+		}
+		velocidad = 5f;
+		debounce = false;
+		termineMov = false;
+		toRotate = pilar.transform.GetChild(1).gameObject;
+		//GetComponent<Renderer>().material.SetColor("_Color", Color.white);
+		GetComponent<SpawnableObject>().ChangeColor(Color.white);
+		target = Quaternion.Euler(0, toRotate.transform.localRotation.eulerAngles.y, 0);
+		lastRotation = 0;
+	}
 
-    // Update is called once per frame
-    void Update()
-    {
-        if (!isServer)
-        {
-            return;
-        }
-        Debug.DrawRay (transform.position, Vector3.up , Color.yellow);
+	// Update is called once per frame
+	void Update()
+	{
+		if (!isServer)
+		{
+			return;
+		}
+		//Debug.DrawRay(transform.position, Vector3.up, Color.yellow);
+		//Debug.DrawRay (transform.position, Vector3.down, Color.yellow);
+		if (termineMov)
+		{
+			//Debug.Log("Termine y puedo entrar");
+			if (!debounce && (Physics.Raycast(transform.position, Vector3.up, 2)))
+			{
+				//Debug.Log("Entro");
+				debounce = true;
+				target = Quaternion.Euler(0, toRotate.transform.localRotation.eulerAngles.y + 90f, 0);
+				//GetComponent<Renderer>().material.SetColor("_Color", Color.green);
+				GetComponent<SpawnableObject>().ChangeColor(Color.green);
+				termineMov = false;
+			}
+			else
+			{
+				if (!Physics.Raycast(transform.position, Vector3.up, 2))
+					debounce = false;
+			}
+		}
+		else
+		{
+		if (toRotate.transform.localRotation != target)
+		{ //Si todavia no llego a destino
+			toRotate.transform.localRotation = Quaternion.Slerp(toRotate.transform.localRotation, target, velocidad * Time.deltaTime);
+			//Debug.Log("Sigo Moviendo");
+		}
+		else
+		{ //Si ya llego a destino..... o esta muy cerca como para detectar una diferencia (Problema de precision)
+			toRotate.transform.localRotation = Quaternion.Euler(0, Mathf.Ceil(toRotate.transform.localRotation.eulerAngles.y), 0);//Lo llevo al numero entero mas cercano
+			termineMov = true; //Termino el movimiento
+							   //GetComponent<Renderer>().material.SetColor("_Color", Color.white);//Cambio color para indicar que ya se puede activar
+			GetComponent<SpawnableObject>().ChangeColor(Color.white);
+		}
+		}
+	}
 
-        if (!captureRotacionTarget) // si no estoy rotando
-        {
-            if ((Physics.Raycast(transform.position, Vector3.up, 2)))//si pase la mano por encima
-            {
-                captureRotacionTarget = true;
-                flagRotacion = true;
-                rotacionTarget = Quaternion.Euler(0, 90*((toRotate.transform.localRotation.eulerAngles.y + 90.0f)/90), 0);//intento forzar multiplos de 90
-                GetComponent<SpawnableObject>().ChangeColor(Color.green);
-            }
-        }
-        else // roto hasta alcanzar el target
-        {             
-            rotacionActual = toRotate.transform.localRotation;
-            if (Math.Abs(rotacionActual.eulerAngles.y- rotacionTarget.eulerAngles.y)>0.00001f)// si todavia no rote lo suficiente
-            {
-                toRotate.transform.localRotation = Quaternion.Slerp(rotacionActual,
-                    rotacionTarget, velocidad * Time.deltaTime);// le mande un 10 para que sea mas rapido ie menos veces
-                Debug.Log("estoy rotando");
-            }
-            else
-            {// si es lo suficientemente igual
-                if (flagRotacion) // si vengo de rotar
-                {
-                    GetComponent<SpawnableObject>().ChangeColor(Color.white);// le vuelvo a poner el color
-                    captureRotacionTarget = false;
-                    flagRotacion = false;
-                }                 
-            }
-        }
-        /*
-        //Debug.DrawRay (transform.position, Vector3.down, Color.yellow);
-        if (termineMov)
-        {
-            //Debug.Log("Termine y puedo entrar");
-            if (!debounce && (Physics.Raycast(transform.position, Vector3.up, 2)))
-            {
-                //Debug.Log("Entro");
-                debounce = true;
-                target = Quaternion.Euler(0, toRotate.transform.localRotation.eulerAngles.y + 90f, 0);
-                //GetComponent<Renderer>().material.SetColor("_Color", Color.green);
-                GetComponent<SpawnableObject>().ChangeColor(Color.green);
-                termineMov = false;
-                flagRotacion = false;
-            }
-            else
-            {
-                if (!Physics.Raycast(transform.position, Vector3.up, 2))
-                    debounce = false;
-            }
-        }
-        else 
-        {
-            if (toRotate.transform.localRotation != target && !termineMov)
-            { //Si todavia no llego a destino
-              //Debug.Log(toRotate.transform.localRotation.eulerAngles);
-                              
-                termineMov = true;                
-                continuarRotando = true;
-                if (continuarRotando)
-                {
-                    toRotate.transform.localRotation = Quaternion.Slerp(toRotate.transform.localRotation, target, velocidad * Time.deltaTime);
-                }
-                //Debug.Log("Sigo Moviendo");
-            }
-            else
-            { //Si ya llego a destino..... o esta muy cerca como para detectar una diferencia (Problema de precision)
-                if (continuarRotando) 
-                {
-                    toRotate.transform.localRotation = Quaternion.Euler(0, Mathf.Ceil(toRotate.transform.localRotation.eulerAngles.y), 0);//Lo llevo al numero entero mas cercano
-                    GetComponent<SpawnableObject>().ChangeColor(Color.white);
-                }                
-                continuarRotando = false;
-                termineMov = true; //Termino el movimiento
-                                   //GetComponent<Renderer>().material.SetColor("_Color", Color.white);//Cambio color para indicar que ya se puede activar
-                
-            }
-        }*/
-
-      
-    }
 }
