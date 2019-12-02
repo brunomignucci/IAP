dbfdfacf793fdee3fb5cc039aeb45dd7f351ce10
nlandau91 <43214457+nlandau91@users.noreply.github.com>
2019-12-02 20:33:27 -0300

    Muchas locuras

---
+++ Refactor/Assets/activadorCasaRara.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class activadorCasaRara : MonoBehaviour
+{
+	// Start is called before the first frame update
+	public GameObject casa;
+	public GameObject casarara;
+
+	private void OnTriggerEnter(Collider other)
+	{
+		toggleActive(other);
+	}
+	void toggleActive(Collider other)
+	{
+		if (other.tag=="PLAYER")
+		{
+			casa.SetActive(!casa.activeSelf);
+			casarara.SetActive(!casarara.activeSelf);
+		}
+		
+	}
+}
